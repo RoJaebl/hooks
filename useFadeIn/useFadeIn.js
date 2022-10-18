034cb4f0bd73196c9af29a499fe524fb3d5a1ee0
RoJaebl <qgs0924@gmail.com>
2022-10-18 22:12:55 +0900

    :memo: create useFadeIn hook

---
+++ useFadeIn/useFadeIn.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+
+/**
+ *
+ * @param {number} duration
+ * @param {number} delay
+ * @returns
+ */
+export default useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const {
+        current: { style },
+      } = element;
+      style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      style.opacity = 1;
+    }
+  }, []);
+  if (typeof duration !== "number" || typeof delay !== "number") {
+    return;
+  }
+  return { ref: element, style: { opacity: 0 } };
+};
